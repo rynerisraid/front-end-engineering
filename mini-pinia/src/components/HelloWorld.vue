d0facd526c8940bc8c3510f5947c9b6d821ebd1b
adminmttt <mingtlin@foxmail.com>
2023-03-21 21:36:24 +0800

    mini-pinia

---
+++ mini-pinia/src/components/HelloWorld.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { reactive,toRefs,computed } from 'vue';
+import { useCounterStore } from '../store/counter'
+
+const counter = useCounterStore();
+
+</script>
+
+<template>
+  <h1>{{ counter.count }} {{ counter.doubleCounter }}</h1>
+  <button @click="counter.count++">counter.count++</button><br />
+  <button @click="counter.inc()">counter.inc()</button><br />
+  <button @click="counter.$patch({ count: counter.count + 1 })">
+          counter.$patch({ count: counter.count + 1 })</button><br />
+  <button @click="counter.$patch((state) => state.count++)">
+          counter.$patch(state => state.count++)
+  </button>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
---
+++ mini-pinia/src/store/counter.js
@@ -0,0 +1,22 @@
+import { defineStore } from "../pinia/store";
+
+
+export const useCounterStore = defineStore('counter',{
+    state(){
+        return {
+            count:1
+        }
+    },
+    getters:{
+        doubleCounter(state){
+            console.log(state)
+            return state.count *2
+        }
+    },
+    actions:{
+        inc(){
+            console.log(this)
+            this.count++
+        }
+    }
+})
